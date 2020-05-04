1d22757ce77739b14665c48fdc95ae388d998a6c
Dmitriy Borodiy <dmitriy.borodiy@gmail.com>
2020-05-03 21:47:51 -0600

    Implement "find largest empty rect" algorithm

--- src/lib/wordart/geometry.ts
+++ src/lib/wordart/geometry.ts
@@ -35,6 +35,13 @@ export const multiplyNoSkew = (m1: Matrix, m2: Matrix): Matrix => {
   }
 }
 
+export const spreadRect = (rect: Rect): [number, number, number, number] => [
+  rect.x,
+  rect.y,
+  rect.w,
+  rect.h,
+]
+
 export type Point = { x: number; y: number }
 export type Rect = { x: number; y: number; w: number; h: number }
 export type Size = { w: number; h: number }
