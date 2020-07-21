ab4be457181377238bade376909c087b7750a2a0
Dmitriy Borodiy <dmitriy.borodiy@gmail.com>
2020-07-21 09:29:10 -0700

    Add react=headroom for desktop header

--- src/components/layouts/SiteLayout/DesktopHeader.tsx
+++ src/components/layouts/SiteLayout/DesktopHeader.tsx
@@ -20,6 +20,7 @@ import { AddIcon, ChevronDownIcon } from '@chakra-ui/icons'
 import { TopNavButton } from 'components/shared/TopNavButton'
 import { useToasts } from 'use-toasts'
 import { MenuItemWithIcon } from 'components/shared/MenuItemWithIcon'
+import Headroom from 'react-headroom'
 
 export type DesktopHeaderProps = {
   fullWidth?: boolean
@@ -39,138 +40,146 @@ export const DesktopHeader: React.FC<DesktopHeaderProps> = observer(
       authStore.isLoggedIn === true && !authStore.isEmailConfirmed
 
     const desktopHeader = (
-      <DesktopHeaderWrapper>
-        <ContentContainer>
-          <Box display="flex" alignItems="center">
-            <Link href="/" passHref>
-              <LogoLink>
-                <img
-                  src="/images/logo-white-text.svg"
-                  css={css`
-                    height: 100%;
-                    width: auto;
-                  `}
-                />
-              </LogoLink>
-            </Link>
+      <Headroom style={{ zIndex: 100 }}>
+        <DesktopHeaderWrapper>
+          <ContentContainer>
+            <Box display="flex" alignItems="center">
+              <Link href="/" passHref>
+                <LogoLink>
+                  <img
+                    src="/images/logo-white-text.svg"
+                    css={css`
+                      height: 100%;
+                      width: auto;
+                    `}
+                  />
+                </LogoLink>
+              </Link>
 
-            <Link href={Urls.pricing} passHref>
-              <TopNavLink active={pathname === Urls.pricing}>
-                Pricing
+              <Link href={Urls.pricing} passHref>
+                <TopNavLink active={pathname === Urls.pricing}>
+                  Pricing
+                </TopNavLink>
+              </Link>
+
+              <Link href={Urls.faq} passHref>
+                <TopNavLink active={pathname === Urls.faq} ml="5">
+                  FAQ
+                </TopNavLink>
+              </Link>
+
+              <Link href={Urls.contact} passHref>
+                <TopNavLink active={pathname === Urls.contact} ml="5">
+                  Contact
+                </TopNavLink>
+              </Link>
+
+              <TopNavLink ml="5" target="_blank" href={Urls.blog}>
+                Blog
               </TopNavLink>
-            </Link>
+            </Box>
 
-            <Link href={Urls.faq} passHref>
-              <TopNavLink active={pathname === Urls.faq} ml="5">
-                FAQ
-              </TopNavLink>
-            </Link>
-
-            <Link href={Urls.contact} passHref>
-              <TopNavLink active={pathname === Urls.contact} ml="5">
-                Contact
-              </TopNavLink>
-            </Link>
-
-            <TopNavLink ml="5" target="_blank" href={Urls.blog}>
-              Blog
-            </TopNavLink>
-          </Box>
-
-          <Box
-            flex="1"
-            justifyContent="flex-end"
-            alignItems="center"
-            display="flex"
-          >
-            {!hideCreate &&
-              pathname !== Urls.yourDesigns &&
-              !isLoggedInAndNotVerified && (
-                <Link href={Urls.editor._next} as={Urls.editor.create} passHref>
-                  <Button colorScheme="accent" leftIcon={<AddIcon />} mr="3">
-                    Create
-                  </Button>
-                </Link>
-              )}
-            {isLoggedIn && (
-              <>
-                {!isLoggedInAndNotVerified && (
-                  <Link href={Urls.yourDesigns} passHref>
-                    <TopNavLink active={pathname === Urls.yourDesigns}>
-                      Your Designs
-                    </TopNavLink>
+            <Box
+              flex="1"
+              justifyContent="flex-end"
+              alignItems="center"
+              display="flex"
+            >
+              {!hideCreate &&
+                pathname !== Urls.yourDesigns &&
+                !isLoggedInAndNotVerified && (
+                  <Link
+                    href={Urls.editor._next}
+                    as={Urls.editor.create}
+                    passHref
+                  >
+                    <Button colorScheme="accent" leftIcon={<AddIcon />} mr="3">
+                      Create
+                    </Button>
                   </Link>
                 )}
+              {isLoggedIn && (
+                <>
+                  {!isLoggedInAndNotVerified && (
+                    <Link href={Urls.yourDesigns} passHref>
+                      <TopNavLink active={pathname === Urls.yourDesigns}>
+                        Your Designs
+                      </TopNavLink>
+                    </Link>
+                  )}
 
-                {/* Account menu */}
-                <Menu isLazy placement="bottom-end">
-                  <MenuButton as={TopNavMenuButton} py="2" px="3">
-                    <Box mr="2">
-                      <FaRegUserCircle />
-                    </Box>
+                  {/* Account menu */}
+                  <Menu isLazy placement="bottom-end">
+                    <MenuButton as={TopNavMenuButton} py="2" px="3">
+                      <Box mr="2">
+                        <FaRegUserCircle />
+                      </Box>
 
-                    <span
-                      css={css`
-                        @media screen and (max-width: 980px) {
-                          display: none;
-                        }
-                      `}
-                    >
-                      Account
-                    </span>
+                      <span
+                        css={css`
+                          @media screen and (max-width: 980px) {
+                            display: none;
+                          }
+                        `}
+                      >
+                        Account
+                      </span>
 
-                    <Box mr="2">
-                      <ChevronDownIcon />
-                    </Box>
-                  </MenuButton>
+                      <Box mr="2">
+                        <ChevronDownIcon />
+                      </Box>
+                    </MenuButton>
 
-                  <Portal>
-                    <MenuTransition>
-                      {(styles) => (
-                        <MenuList
-                          // @ts-ignore
-                          css={styles}
-                        >
-                          <MenuItemWithIcon
-                            icon={<FaRegUserCircle />}
-                            title="Your account"
-                            onClick={() => {
-                              router.push(Urls.account)
-                            }}
-                          />
-                          <MenuItemWithIcon
-                            icon={<FaSignOutAlt />}
-                            title="Log out"
-                            onClick={() => {
-                              authStore.logout()
-                              toasts.showInfo({ title: 'You have logged out' })
-                              router.replace(Urls.login)
-                            }}
-                          />
-                        </MenuList>
-                      )}
-                    </MenuTransition>
-                  </Portal>
-                </Menu>
-              </>
-            )}
-            {isNotLoggedIn && (
-              <>
-                <Link href={Urls.signup} passHref>
-                  <TopNavButton variant="accent" mr="3">
-                    Sign up
-                  </TopNavButton>
-                </Link>
-                <Link href={Urls.login} passHref>
-                  <TopNavLink active={pathname === Urls.login}>
-                    Log in
-                  </TopNavLink>
-                </Link>
-              </>
-            )}
-          </Box>
-        </ContentContainer>
-      </DesktopHeaderWrapper>
+                    <Portal>
+                      <MenuTransition>
+                        {(styles) => (
+                          <MenuList
+                            // @ts-ignore
+                            css={styles}
+                          >
+                            <MenuItemWithIcon
+                              icon={<FaRegUserCircle />}
+                              title="Your account"
+                              onClick={() => {
+                                router.push(Urls.account)
+                              }}
+                            />
+                            <MenuItemWithIcon
+                              icon={<FaSignOutAlt />}
+                              title="Log out"
+                              onClick={() => {
+                                authStore.logout()
+                                toasts.showInfo({
+                                  title: 'You have logged out',
+                                })
+                                router.replace(Urls.login)
+                              }}
+                            />
+                          </MenuList>
+                        )}
+                      </MenuTransition>
+                    </Portal>
+                  </Menu>
+                </>
+              )}
+              {isNotLoggedIn && (
+                <>
+                  <Link href={Urls.signup} passHref>
+                    <TopNavButton variant="accent" mr="3">
+                      Sign up
+                    </TopNavButton>
+                  </Link>
+                  <Link href={Urls.login} passHref>
+                    <TopNavLink active={pathname === Urls.login}>
+                      Log in
+                    </TopNavLink>
+                  </Link>
+                </>
+              )}
+            </Box>
+          </ContentContainer>
+        </DesktopHeaderWrapper>
+      </Headroom>
     )
 
     return <>{desktopHeader}</>
