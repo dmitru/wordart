ced79b6a0e8eeb6b1a522e3b6fd07ac7b0029c2c
Dmitriy Borodiy <dmitriy.borodiy@gmail.com>
2020-07-22 13:55:04 -0700

    Refactor resize left tab

--- src/components/Editor/components/Editor.tsx
+++ src/components/Editor/components/Editor.tsx
@@ -51,6 +51,7 @@ import { LeftPanelFontsTab } from 'components/Editor/components/LeftPanelFontsTa
 import { LeftPanelIconsTab } from 'components/Editor/components/LeftPanelIconsTab'
 import { LeftPanelLayoutTab } from 'components/Editor/components/LeftPanelLayoutTab'
 import { LeftPanelWordsTab } from 'components/Editor/components/LeftPanelWordsTab'
+import { LeftPanelResizeTab } from 'components/Editor/components/LeftPanelResizeTab '
 import { LeftPanelShapesTab } from 'components/Editor/components/ShapesTab/LeftPanelShapesTab'
 import { Spinner } from 'components/Editor/components/Spinner'
 import { WarningModal } from 'components/Editor/components/WarningModal'
@@ -762,142 +763,7 @@ export const EditorComponent: React.FC<EditorComponentProps> = observer(
                   {store.lifecycleState === 'initialized' ? (
                     <>
                       {state.leftPanelContext === 'resize' && (
-                        <Box px="3" py="5">
-                          <SectionLabel>Page size</SectionLabel>
-
-                          <Box display="flex" flexWrap="wrap">
-                            {pageSizePresets.map((preset) => (
-                              <Button
-                                variant="outline"
-                                outline={
-                                  store.pageSize.kind === 'preset' &&
-                                  store.pageSize.preset.id === preset.id
-                                    ? '3px solid hsl(358, 80%, 65%) !important'
-                                    : undefined
-                                }
-                                mr="2"
-                                mb="3"
-                                key={preset.id}
-                                onClick={() => {
-                                  store.setPageSize({ kind: 'preset', preset })
-                                  store.animateVisualize(false)
-                                }}
-                                display="flex"
-                                flexDirection="column"
-                                justifyContent="center"
-                                width="160px"
-                                minHeight="70px"
-                              >
-                                <Text fontSize="md">{preset.title}</Text>
-                                <Text my="0" fontSize="xs" color="gray.500">
-                                  {preset.subtitle}
-                                </Text>
-                              </Button>
-                            ))}
-                            <Button
-                              variant="outline"
-                              outline={
-                                store.pageSize.kind === 'custom'
-                                  ? '3px solid hsl(358, 80%, 65%) !important'
-                                  : undefined
-                              }
-                              mr="2"
-                              mb="3"
-                              display="flex"
-                              flexDirection="column"
-                              justifyContent="center"
-                              width="160px"
-                              minHeight="70px"
-                              onClick={() => {
-                                store.setPageSize({
-                                  kind: 'custom',
-                                })
-                              }}
-                            >
-                              <Text fontSize="md">Custom</Text>
-                              <Text my="0" fontSize="xs" color="gray.500">
-                                Choose your own
-                              </Text>
-                            </Button>
-                          </Box>
-
-                          {/* Custom aspect ratio input */}
-                          {store.pageSize.kind === 'custom' && (
-                            <>
-                              <Box mt="5">
-                                <SectionLabel>Custom aspect ratio</SectionLabel>
-                              </Box>
-
-                              <Stack
-                                spacing="2"
-                                mt="5"
-                                mb="4"
-                                direction="row"
-                                alignItems="flex-end"
-                              >
-                                <Box>
-                                  <Text mb="1">Width</Text>
-                                  <NumberInput
-                                    step={1}
-                                    value={store.pageSize.custom.width}
-                                    min={1}
-                                    max={8192}
-                                    onChange={(value) => {
-                                      store.pageSize.custom.width = value
-                                    }}
-                                  >
-                                    <NumberInputField />
-                                    <NumberInputStepper>
-                                      <NumberIncrementStepper />
-                                      <NumberDecrementStepper />
-                                    </NumberInputStepper>
-                                  </NumberInput>
-                                </Box>
-
-                                <Box>
-                                  <Text mb="1">Height</Text>
-                                  <NumberInput
-                                    step={1}
-                                    value={store.pageSize.custom.height}
-                                    min={1}
-                                    max={8192}
-                                    onChange={(value) => {
-                                      store.pageSize.custom.height = value
-                                    }}
-                                  >
-                                    <NumberInputField />
-                                    <NumberInputStepper>
-                                      <NumberIncrementStepper />
-                                      <NumberDecrementStepper />
-                                    </NumberInputStepper>
-                                  </NumberInput>
-                                </Box>
-
-                                <Button
-                                  colorScheme="primary"
-                                  onClick={() => {
-                                    store.setPageSize({})
-                                  }}
-                                >
-                                  Update
-                                </Button>
-                              </Stack>
-                            </>
-                          )}
-
-                          <Box>
-                            <Button
-                              mt="4"
-                              width="100%"
-                              colorScheme="accent"
-                              onClick={() => {
-                                state.leftPanelContext = 'normal'
-                              }}
-                            >
-                              Done
-                            </Button>
-                          </Box>
-                        </Box>
+                        <LeftPanelResizeTab />
                       )}
 
                       {state.leftPanelContext === 'normal' && (
---
+++ src/components/Editor/components/LeftPanelResizeTab.tsx
@@ -0,0 +1,160 @@
+import {
+  Box,
+  Button,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  Stack,
+  Text,
+} from '@chakra-ui/core'
+import { pageSizePresets } from 'components/Editor/editor-store'
+import { observer } from 'mobx-react'
+import React from 'react'
+import { WordcloudId } from 'services/api/types'
+import { useStore } from 'services/root-store'
+import { SectionLabel } from './shared'
+
+export const LeftPanelResizeTab: React.FC<{}> = observer((props) => {
+  const { editorPageStore: store, wordcloudsStore } = useStore()
+
+  return (
+    <Box px="3" py="5">
+      <SectionLabel>Page size</SectionLabel>
+
+      <Box display="flex" flexWrap="wrap">
+        {pageSizePresets.map((preset) => (
+          <Button
+            variant="outline"
+            outline={
+              store.pageSize.kind === 'preset' &&
+              store.pageSize.preset.id === preset.id
+                ? '3px solid hsl(358, 80%, 65%) !important'
+                : undefined
+            }
+            mr="2"
+            mb="3"
+            key={preset.id}
+            onClick={() => {
+              store.setPageSize({ kind: 'preset', preset })
+              store.animateVisualize(false)
+            }}
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            width="160px"
+            minHeight="70px"
+          >
+            <Text fontSize="md">{preset.title}</Text>
+            <Text my="0" fontSize="xs" color="gray.500">
+              {preset.subtitle}
+            </Text>
+          </Button>
+        ))}
+        <Button
+          variant="outline"
+          outline={
+            store.pageSize.kind === 'custom'
+              ? '3px solid hsl(358, 80%, 65%) !important'
+              : undefined
+          }
+          mr="2"
+          mb="3"
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          width="160px"
+          minHeight="70px"
+          onClick={() => {
+            store.setPageSize({
+              kind: 'custom',
+            })
+          }}
+        >
+          <Text fontSize="md">Custom</Text>
+          <Text my="0" fontSize="xs" color="gray.500">
+            Choose your own
+          </Text>
+        </Button>
+      </Box>
+
+      {/* Custom aspect ratio input */}
+      {store.pageSize.kind === 'custom' && (
+        <>
+          <Box mt="5">
+            <SectionLabel>Custom aspect ratio</SectionLabel>
+          </Box>
+
+          <Stack
+            spacing="2"
+            mt="5"
+            mb="4"
+            direction="row"
+            alignItems="flex-end"
+          >
+            <Box>
+              <Text mb="1">Width</Text>
+              <NumberInput
+                step={1}
+                value={store.pageSize.custom.width}
+                min={1}
+                max={8192}
+                onChange={(value) => {
+                  store.pageSize.custom.width = value
+                }}
+              >
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </Box>
+
+            <Box>
+              <Text mb="1">Height</Text>
+              <NumberInput
+                step={1}
+                value={store.pageSize.custom.height}
+                min={1}
+                max={8192}
+                onChange={(value) => {
+                  store.pageSize.custom.height = value
+                }}
+              >
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </Box>
+
+            <Button
+              colorScheme="primary"
+              onClick={() => {
+                store.setPageSize({})
+              }}
+            >
+              Update
+            </Button>
+          </Stack>
+        </>
+      )}
+
+      <Box>
+        <Button
+          mt="4"
+          width="100%"
+          colorScheme="accent"
+          onClick={() => {
+            state.leftPanelContext = 'normal'
+          }}
+        >
+          Done
+        </Button>
+      </Box>
+    </Box>
+  )
+})
