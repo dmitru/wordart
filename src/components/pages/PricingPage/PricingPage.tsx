9a31f788aca2124f8ebf5bd0159b7a76f506a240
Dmitriy Borodiy <dmitriy.borodiy@gmail.com>
2020-07-27 09:27:26 -0700

    WIP: upgrade modal, show plans

--- src/components/pages/PricingPage/PricingPage.tsx
+++ src/components/pages/PricingPage/PricingPage.tsx
@@ -1,24 +1,16 @@
-import { Box, Button, Text } from '@chakra-ui/core'
+import { Box, Text } from '@chakra-ui/core'
 import css from '@emotion/css'
 import styled from '@emotion/styled'
 import { SiteLayout } from 'components/layouts/SiteLayout/SiteLayout'
 import { HelpTooltipIcon } from 'components/shared/HelpTooltipIcon'
 import 'lib/wordart/console-extensions'
 import { observer } from 'mobx-react'
-import { downloadsPricingPlans, unlimitedPricingPlans } from 'plans'
-import React, { useState } from 'react'
+import React from 'react'
 import { Helmet } from 'react-helmet'
-import { useStore } from 'services/root-store'
 import { getTabTitle } from 'utils/tab-title'
+import { PricingPlans } from './PricingPlans'
 
 export const PricingPage = observer(() => {
-  const { authStore } = useStore()
-  const { profile } = authStore
-
-  const [selectedPlanId, setSelectedPlanId] = useState(
-    unlimitedPricingPlans[0].id
-  )
-
   const noLimitsHelpIcon = (
     <HelpTooltipIcon label="Enjoy no limits as long as there are unused HQ downloads in your account + 1 week after that" />
   )
@@ -172,198 +164,6 @@ export const PricingPage = observer(() => {
     </Box>
   )
 
-  const unlimitedPlans = (
-    <Box
-      mt={['1rem', '1rem', '3rem']}
-      mb={['1rem', '1rem', '1rem']}
-      mx={[2, 6]}
-    >
-      <Box width={['300px', '340px', '340px', '360px']}>
-        <Box boxShadow="lg" borderRadius="lg">
-          <Box
-            p="5"
-            fontSize="xl"
-            bg="gray.100"
-            textAlign="center"
-            borderRadius="lg"
-          >
-            Unlimited Plans
-          </Box>
-
-          <Box as="ul" color="gray.500" mt="5" pr="4" fontSize={['sm', 'md']}>
-            <li>Unlimited number of HQ downloads</li>
-            <li>Commercial use {commercialUseHelp}</li>
-            <li>
-              More advanced features (
-              <a href="#pricing-comparison">Learn more</a>)
-            </li>
-          </Box>
-
-          <Box p="5" pt={['0', '3']}>
-            {unlimitedPricingPlans.map((plan) => {
-              const price = authStore.planPrices.get(plan.id)
-              return (
-                <Box
-                  px="3"
-                  py={['3']}
-                  display="flex"
-                  alignItems="center"
-                  key={plan.id}
-                  onClick={() => {
-                    setSelectedPlanId(plan.id)
-                  }}
-                  _hover={{
-                    bg: plan.id === selectedPlanId ? 'gray.100' : 'gray.50',
-                  }}
-                  bg={plan.id === selectedPlanId ? 'primary.50' : 'white'}
-                  css={css`
-                    cursor: pointer;
-                  `}
-                  borderRadius="lg"
-                >
-                  <Text my="0" flex="1" mr="3" fontSize={['sm', 'md', 'lg']}>
-                    {plan.title}
-                  </Text>
-
-                  <>
-                    <Text
-                      my="0"
-                      fontWeight="semibold"
-                      fontSize={['sm', 'md', 'xl']}
-                    >
-                      ${price?.price?.net ?? 0}
-                    </Text>
-                    <Text my="0" ml="3" fontSize="sm" color="gray.500">
-                      {price?.currency ?? 'USD'}
-                    </Text>
-                  </>
-                </Box>
-              )
-            })}
-
-            <Box mt="5">
-              <Button
-                width="100%"
-                size="lg"
-                colorScheme="accent"
-                onClick={() => {
-                  window.Paddle.Checkout.open({
-                    product: selectedPlanId,
-                    email: profile?.email,
-                  })
-                }}
-              >
-                Sign up
-              </Button>
-            </Box>
-          </Box>
-        </Box>
-        <Text color="gray.400" fontSize="sm" mt="5" mx="3">
-          * All plans are one-time payment, not a recurring subscription.
-        </Text>
-      </Box>
-    </Box>
-  )
-
-  const downloadPlans = (
-    <Box
-      mt={['2rem', '2rem', '3rem']}
-      mb={['2rem', '2rem', '2rem']}
-      mx={[2, 6]}
-    >
-      <Box width={['300px', '340px', '340px', '360px']}>
-        <Box boxShadow="lg" borderRadius="lg">
-          <Box
-            p="5"
-            fontSize={['lg', 'lg', 'xl']}
-            bg="gray.100"
-            textAlign="center"
-            borderRadius="lg"
-          >
-            High Quality Download Packs
-          </Box>
-
-          <Box as="ul" color="gray.500" mt="5" pr="4" fontSize={['sm', 'md']}>
-            <li>
-              Fixed number of HQ downloads
-              <HelpTooltipIcon
-                mr="3"
-                css={css`
-                  top: 4x;
-                  position: relative;
-                `}
-                label="HQ downloads never expire. Only unique downloads are counted: you may download the same design multiple times in different formats for free, as long as the design doesn't change. "
-              />
-            </li>
-            <li>Commercial use {commercialUseHelp}</li>
-            <li>
-              More advanced features (<a href="#pricing-faq">Learn more</a>)
-            </li>
-          </Box>
-
-          <Box p="5" pt={['0', '3']}>
-            {downloadsPricingPlans.map((plan) => {
-              const price = authStore.planPrices.get(plan.id)
-              return (
-                <Box
-                  px="3"
-                  py={['3']}
-                  display="flex"
-                  alignItems="center"
-                  key={plan.id}
-                  onClick={() => {
-                    setSelectedPlanId(plan.id)
-                  }}
-                  _hover={{
-                    bg: plan.id === selectedPlanId ? 'gray.100' : 'gray.50',
-                  }}
-                  bg={plan.id === selectedPlanId ? 'primary.50' : 'white'}
-                  css={css`
-                    cursor: pointer;
-                  `}
-                  borderRadius="lg"
-                >
-                  <Text my="0" flex="1" mr="3" fontSize={['sm', 'md', 'lg']}>
-                    {plan.title}
-                  </Text>
-
-                  <>
-                    <Text
-                      my="0"
-                      fontWeight="semibold"
-                      fontSize={['sm', 'md', 'xl']}
-                    >
-                      ${price?.price?.net ?? 0}
-                    </Text>
-                    <Text my="0" ml="3" fontSize="sm" color="gray.500">
-                      {price?.currency ?? 'USD'}
-                    </Text>
-                  </>
-                </Box>
-              )
-            })}
-
-            <Box mt="5">
-              <Button
-                width="100%"
-                size="lg"
-                colorScheme="accent"
-                onClick={() => {
-                  window.Paddle.Checkout.open({
-                    product: selectedPlanId,
-                    email: profile?.email,
-                  })
-                }}
-              >
-                Sign up
-              </Button>
-            </Box>
-          </Box>
-        </Box>
-      </Box>
-    </Box>
-  )
-
   return (
     <SiteLayout>
       <Box>
@@ -395,16 +195,7 @@ export const PricingPage = observer(() => {
             </Text>
           </Box>
 
-          <Box
-            display="flex"
-            mx="auto"
-            alignItems={['center', 'center', 'flex-start']}
-            justifyContent="center"
-            flexDirection={['column', 'column', 'row']}
-          >
-            {downloadPlans}
-            {unlimitedPlans}
-          </Box>
+          <PricingPlans />
 
           {comparisonSection}
 
---
+++ src/components/upgrade/UpgradeModal.tsx
@@ -0,0 +1,85 @@
+import {
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+} from '@chakra-ui/core'
+import { observable, runInAction } from 'mobx'
+import { observer } from 'mobx-react'
+import { PricingPlans } from 'components/pages/PricingPage/PricingPlans'
+import css from '@emotion/css'
+
+export type UpgradeModalVariant =
+  | 'generic'
+  | 'hq-download'
+  | 'folder-limits'
+  | 'design-limits'
+  | 'custom-fonts'
+  | 'custom-images'
+
+export type UpgradeModalProps = {
+  variant: UpgradeModalVariant
+  isOpen: boolean
+  onClose: () => void
+}
+
+export const UpgradeModal: React.FC<UpgradeModalProps> = observer(
+  ({ variant, isOpen, onClose }) => {
+    return (
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay>
+          <ModalContent
+            css={css`
+              margin-left: 20px;
+              margin-right: 20px;
+              width: 100%;
+              max-width: 900px;
+            `}
+          >
+            <ModalHeader>Upgrade: {variant}</ModalHeader>
+
+            <ModalBody>
+              <PricingPlans />
+            </ModalBody>
+
+            <ModalCloseButton />
+          </ModalContent>
+        </ModalOverlay>
+      </Modal>
+    )
+  }
+)
+
+const state = observable({
+  isOpen: false,
+  variant: null as UpgradeModalVariant | null,
+})
+
+export const UpgradeModalContainer = observer(() => {
+  const { isOpen, variant } = state
+  return (
+    <UpgradeModal
+      isOpen={isOpen}
+      variant={variant || 'generic'}
+      onClose={() => {
+        state.isOpen = false
+      }}
+    />
+  )
+})
+
+export const useUpgradeModal = () => {
+  return {
+    show: (variant: UpgradeModalVariant) => {
+      runInAction(() => {
+        state.variant = variant
+        state.isOpen = true
+      })
+    },
+    hide: () => {
+      state.isOpen = false
+    },
+  }
+}
