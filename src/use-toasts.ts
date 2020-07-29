7d1808ef855d52c585aae5c87da4bea418d90d7d
Dmitriy Borodiy <dmitriy.borodiy@gmail.com>
2020-07-29 09:02:32 -0700

    Fix success toasts not showing text content

--- src/use-toasts.ts
+++ src/use-toasts.ts
@@ -24,7 +24,7 @@ export const useToasts = () => {
       toasts({
         ...commonParams,
         status: 'success',
-        // ...params,
+        ...params,
       }),
     showError: (params: UseToastOptions) =>
       toasts({
