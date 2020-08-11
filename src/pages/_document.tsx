416c762607d7e0d279d4f12ad24421616ddeceaa
Dmitriy Borodiy <dmitriy.borodiy@gmail.com>
2020-08-11 10:38:34 -0700

    Attempt to fix FUOC

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,25 @@
 import { config } from 'config'
 import Document, { Head, Main, NextScript } from 'next/document'
+import { extractCritical } from 'emotion-server'
 
 export default class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const initialProps = await Document.getInitialProps(ctx)
+    const styles = extractCritical(initialProps.html)
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          <style
+            data-emotion-css={styles.ids.join(' ')}
+            dangerouslySetInnerHTML={{ __html: styles.css }}
+          />
+        </>
+      ),
+    }
+  }
+
   getGoogleAnalyticsTags() {
     return {
       __html: `
