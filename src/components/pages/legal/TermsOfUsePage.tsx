a4c825e9f419b9433d914f58306a3456e716209a
Dmitriy Borodiy <dmitriy.borodiy@gmail.com>
2020-08-11 18:00:48 -0700

    Add content to legal pages

--- src/components/pages/legal/TermsOfUsePage.tsx
+++ src/components/pages/legal/TermsOfUsePage.tsx
@@ -10,19 +10,250 @@ export const TermsOfUsePage = observer(() => {
       <Box>
         <NextSeo noindex={true} title="Terms of Use" />
 
-        <Box mb="6rem">
-          <Box
-            display="flex"
-            flexDirection="column"
-            maxWidth="600px"
-            textAlign="center"
-            mx="auto"
+        <h1>Terms and conditions</h1>
+        <p>
+          These terms and conditions (&quot;Terms&quot;, &quot;Agreement&quot;)
+          are an agreement between Website Operator (&quot;Website
+          Operator&quot;, &quot;us&quot;, &quot;we&quot; or &quot;our&quot;) and
+          you (&quot;User&quot;, &quot;you&quot; or &quot;your&quot;). This
+          Agreement sets forth the general terms and conditions of your use of
+          the{' '}
+          <a target="_blank" rel="nofollow" href="https://wordcloudy.com">
+            wordcloudy.com
+          </a>{' '}
+          website and any of its products or services (collectively,
+          &quot;Website&quot; or &quot;Services&quot;).
+        </p>
+        <h2>Links to other websites</h2>
+        <p>
+          Although this Website may link to other websites, we are not, directly
+          or indirectly, implying any approval, association, sponsorship,
+          endorsement, or affiliation with any linked website, unless
+          specifically stated herein. Some of the links on the Website may be
+          &quot;affiliate links&quot;. This means if you click on the link and
+          purchase an item, Website Operator will receive an affiliate
+          commission. We are not responsible for examining or evaluating, and we
+          do not warrant the offerings of, any businesses or individuals or the
+          content of their websites. We do not assume any responsibility or
+          liability for the actions, products, services, and content of any
+          other third-parties. You should carefully review the legal statements
+          and other conditions of use of any website which you access through a
+          link from this Website. Your linking to any other off-site websites is
+          at your own risk.
+        </p>
+        <h2>Prohibited uses</h2>
+        <p>
+          In addition to other terms as set forth in the Agreement, you are
+          prohibited from using the Website or its Content: (a) for any unlawful
+          purpose; (b) to solicit others to perform or participate in any
+          unlawful acts; (c) to violate any international, federal, provincial
+          or state regulations, rules, laws, or local ordinances; (d) to
+          infringe upon or violate our intellectual property rights or the
+          intellectual property rights of others; (e) to harass, abuse, insult,
+          harm, defame, slander, disparage, intimidate, or discriminate based on
+          gender, sexual orientation, religion, ethnicity, race, age, national
+          origin, or disability; (f) to submit false or misleading information;
+          (g) to upload or transmit viruses or any other type of malicious code
+          that will or may be used in any way that will affect the functionality
+          or operation of the Service or of any related website, other websites,
+          or the Internet; (h) to collect or track the personal information of
+          others; (i) to spam, phish, pharm, pretext, spider, crawl, or scrape;
+          (j) for any obscene or immoral purpose; or (k) to interfere with or
+          circumvent the security features of the Service or any related
+          website, other websites, or the Internet. We reserve the right to
+          terminate your use of the Service or any related website for violating
+          any of the prohibited uses.
+        </p>
+        <h2>Intellectual property rights</h2>
+        <p>
+          This Agreement does not transfer to you any intellectual property
+          owned by Website Operator or third-parties, and all rights, titles,
+          and interests in and to such property will remain (as between the
+          parties) solely with Website Operator. All trademarks, service marks,
+          graphics and logos used in connection with our Website or Services,
+          are trademarks or registered trademarks of Website Operator or Website
+          Operator licensors. Other trademarks, service marks, graphics and
+          logos used in connection with our Website or Services may be the
+          trademarks of other third-parties. Your use of our Website and
+          Services grants you no right or license to reproduce or otherwise use
+          any Website Operator or third-party trademarks.
+        </p>
+        <h2>Limitation of liability</h2>
+        <p>
+          To the fullest extent permitted by applicable law, in no event will
+          Website Operator, its affiliates, officers, directors, employees,
+          agents, suppliers or licensors be liable to any person for (a): any
+          indirect, incidental, special, punitive, cover or consequential
+          damages (including, without limitation, damages for lost profits,
+          revenue, sales, goodwill, use of content, impact on business, business
+          interruption, loss of anticipated savings, loss of business
+          opportunity) however caused, under any theory of liability, including,
+          without limitation, contract, tort, warranty, breach of statutory
+          duty, negligence or otherwise, even if Website Operator has been
+          advised as to the possibility of such damages or could have foreseen
+          such damages. To the maximum extent permitted by applicable law, the
+          aggregate liability of Website Operator and its affiliates, officers,
+          employees, agents, suppliers and licensors, relating to the services
+          will be limited to an amount greater of one dollar or any amounts
+          actually paid in cash by you to Website Operator for the prior one
+          month period prior to the first event or occurrence giving rise to
+          such liability. The limitations and exclusions also apply if this
+          remedy does not fully compensate you for any losses or fails of its
+          essential purpose.
+        </p>
+        <h2>Indemnification</h2>
+        <p>
+          You agree to indemnify and hold Website Operator and its affiliates,
+          directors, officers, employees, and agents harmless from and against
+          any liabilities, losses, damages or costs, including reasonable
+          attorneys' fees, incurred in connection with or arising from any
+          third-party allegations, claims, actions, disputes, or demands
+          asserted against any of them as a result of or relating to your
+          Content, your use of the Website or Services or any willful misconduct
+          on your part.
+        </p>
+        <h2>Severability</h2>
+        <p>
+          All rights and restrictions contained in this Agreement may be
+          exercised and shall be applicable and binding only to the extent that
+          they do not violate any applicable laws and are intended to be limited
+          to the extent necessary so that they will not render this Agreement
+          illegal, invalid or unenforceable. If any provision or portion of any
+          provision of this Agreement shall be held to be illegal, invalid or
+          unenforceable by a court of competent jurisdiction, it is the
+          intention of the parties that the remaining provisions or portions
+          thereof shall constitute their agreement with respect to the subject
+          matter hereof, and all such remaining provisions or portions thereof
+          shall remain in full force and effect.
+        </p>
+        <h2>Changes and amendments</h2>
+        <p>
+          We reserve the right to modify this Agreement or its policies relating
+          to the Website or Services at any time, effective upon posting of an
+          updated version of this Agreement on the Website. When we do, we will
+          revise the updated date at the bottom of this page. Continued use of
+          the Website after any such changes shall constitute your consent to
+          such changes.
+        </p>
+        <h2>Acceptance of these terms</h2>
+        <p>
+          You acknowledge that you have read this Agreement and agree to all its
+          terms and conditions. By using the Website or its Services you agree
+          to be bound by this Agreement. If you do not agree to abide by the
+          terms of this Agreement, you are not authorized to use or access the
+          Website and its Services.
+        </p>
+        <h2>Contacting us</h2>
+        <p>
+          If you would like to contact us to understand more about this
+          Agreement or wish to contact us concerning any matter relating to it,
+          you may do so via the{' '}
+          <a
+            target="_blank"
+            rel="nofollow"
+            href="https://docs.google.com/forms/d/e/1FAIpQLSfvQlbh4TfuTg47Q0AZgJUc6adz4DEi5JwN6k1_4SBfUp1x8g/viewform?usp=sf_link"
           >
-            <Text as="h1" textAlign="center">
-              TODO
-            </Text>
-          </Box>
-        </Box>
+            contact form
+          </a>
+        </p>
+        <p>This document was last updated on March 10, 2020</p>
+
+        <h1>Disclaimer</h1>
+        <p>
+          This disclaimer (&quot;Disclaimer&quot;, &quot;Agreement&quot;) is an
+          agreement between Website Operator (&quot;Website Operator&quot;,
+          &quot;us&quot;, &quot;we&quot; or &quot;our&quot;) and you
+          (&quot;User&quot;, &quot;you&quot; or &quot;your&quot;). This
+          Disclaimer sets forth the general guidelines, terms and conditions of
+          your use of the{' '}
+          <a target="_blank" rel="nofollow" href="https://myguitartuner.com">
+            myguitartuner.com
+          </a>{' '}
+          website and any of its products or services (collectively,
+          &quot;Website&quot; or &quot;Services&quot;).
+        </p>
+        <h2>Representation</h2>
+        <p>
+          Any views or opinions represented in this Website belong solely to the
+          Content creators and do not represent those of people, institutions or
+          organizations that the Website Operator or creators may or may not be
+          associated with in professional or personal capacity, unless
+          explicitly stated. Any views or opinions are not intended to malign
+          any religion, ethnic group, club, organization, company, or
+          individual.
+        </p>
+        <h2>Content and postings</h2>
+        <p>
+          You may not modify, print or copy any part of the Website. Inclusion
+          of any part of this Website in another work, whether in printed or
+          electronic or another form or inclusion of any part of the Website in
+          another website by embedding, framing or otherwise without the express
+          permission of Website Operator is prohibited.{' '}
+        </p>
+        <h2>Compensation</h2>
+        <p>
+          Some of the links on the Website may be &quot;affiliate links&quot;.
+          This means if you click on the link and purchase an item, Website
+          Operator will receive an affiliate commission.
+        </p>
+        <h2>Indemnification and warranties</h2>
+        <p>
+          While we have made every attempt to ensure that the information
+          contained on the Website is correct, Website Operator is not
+          responsible for any errors or omissions, or for the results obtained
+          from the use of this information. All information on the Website is
+          provided &quot;as is&quot;, with no guarantee of completeness,
+          accuracy, timeliness or of the results obtained from the use of this
+          information, and without warranty of any kind, express or implied. In
+          no event will Website Operator, or its partners, employees or agents,
+          be liable to you or anyone else for any decision made or action taken
+          in reliance on the information on the Website or for any
+          consequential, special or similar damages, even if advised of the
+          possibility of such damages. Information on the Website is for general
+          information purposes only and is not intended to provide legal,
+          financial, medical, or any other type of professional advice. Please
+          seek professional assistance should you require it. Furthermore,
+          information contained on the Website and any pages linked to and from
+          it are subject to change at any time and without warning.
+        </p>
+        <p>
+          We reserve the right to modify this Disclaimer relating to the Website
+          or Services at any time, effective upon posting of an updated version
+          of this Disclaimer on the Website. When we do we will revise the
+          updated date at the bottom of this page. Continued use of the Website
+          after any such changes shall constitute your consent to such changes.
+          Policy was created with{' '}
+          <a
+            target="_blank"
+            title="Sample disclaimer template"
+            href="https://www.websitepolicies.com/disclaimer-generator"
+          >
+            WebsitePolicies
+          </a>
+          .
+        </p>
+        <h2>Acceptance of this disclaimer</h2>
+        <p>
+          You acknowledge that you have read this Disclaimer and agree to all
+          its terms and conditions. By accessing the Website you agree to be
+          bound by this Disclaimer. If you do not agree to abide by the terms of
+          this Disclaimer, you are not authorized to use or access the Website.
+        </p>
+        <h2>Contacting us</h2>
+        <p>
+          If you would like to contact us to understand more about this
+          Disclaimer or wish to contact us concerning any matter relating to it,
+          you may do so via the{' '}
+          <a
+            target="_blank"
+            rel="nofollow"
+            href="https://wordcloudy.com/contact"
+          >
+            contact form
+          </a>{' '}
+          or sending us an email to support[at]wordcloudy.com
+        </p>
+        <p>This document was last updated on March 10, 2020</p>
       </Box>
     </SiteLayout>
   )
